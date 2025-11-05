5afeed8c31f0d31ebe060cc51f6075553fa07950
Tuyisenge Jordan <jordanale.112@gmail.com>
2025-11-05 21:17:26 +0200

    task 14

---
+++ math/0x00-linear_algebra/14-saddle_up.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Author: Jordan Tuyisenge"""
+import numpy as np
+
+def np_matmul(mat1, mat2):
+    """Performs matrix multiplication of two numpy.ndarrays"""
+    return np.matmul(mat1, mat2)
+
