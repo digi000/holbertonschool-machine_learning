c825f22419e11157f88536f70087b21b2e705837
Tuyisenge Jordan <jordanale.112@gmail.com>
2025-11-05 21:16:45 +0200

    task 12

---
+++ math/0x00-linear_algebra/12-bracin_the_elements.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+"""Author: Jordan Tuyisenge"""
+
+def np_elementwise(mat1, mat2):
+    """Performs element-wise addition, subtraction, multiplication, and division"""
+    return (mat1 + mat2, mat1 - mat2, mat1 * mat2, mat1 / mat2)
